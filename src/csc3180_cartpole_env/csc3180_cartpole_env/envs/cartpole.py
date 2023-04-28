e259a0f859130f39ac5bc0b9366a9e08bb5d27a0
ThomasXU-17 <xuanyangxu@link.cuhk.edu.cn>
2023-04-28 22:10:26 +0800

    add text

--- src/csc3180_cartpole_env/csc3180_cartpole_env/envs/cartpole.py
+++ src/csc3180_cartpole_env/csc3180_cartpole_env/envs/cartpole.py
@@ -19,47 +19,65 @@ from gymnasium.vector.utils import batch_space
 class CartPoleEnv_CSC3180(gym.Env[np.ndarray, Union[int, np.ndarray]]):
     """
     ## Description
+
     This environment corresponds to the version of the cart-pole problem described by Barto, Sutton, and Anderson in
     ["Neuronlike Adaptive Elements That Can Solve Difficult Learning Control Problem"](https://ieeexplore.ieee.org/document/6313077).
     A pole is attached by an un-actuated joint to a cart, which moves along a frictionless track.
     The pendulum is placed upright on the cart and the goal is to balance the pole by applying forces
      in the left and right direction on the cart.
+
     ## Action Space
+
     The action is a `ndarray` with shape `(1,)` which can take values `{0, 1}` indicating the direction
      of the fixed force the cart is pushed with.
+
     - 0: Push cart to the left
     - 1: Push cart to the right
+
     **Note**: The velocity that is reduced or increased by the applied force is not fixed and it depends on the angle
      the pole is pointing. The center of gravity of the pole varies the amount of energy needed to move the cart underneath it
+    
     ## Observation Space
+
     The observation is a `ndarray` with shape `(4,)` with the values corresponding to the following positions and velocities:
+    
     | Num | Observation           | Min                 | Max               |
     |-----|-----------------------|---------------------|-------------------|
     | 0   | Cart Position         | -4.8                | 4.8               |
     | 1   | Cart Velocity         | -Inf                | Inf               |
     | 2   | Pole Angle            | ~ -0.418 rad (-24°) | ~ 0.418 rad (24°) |
     | 3   | Pole Angular Velocity | -Inf                | Inf               |
+
     **Note:** While the ranges above denote the possible values for observation space of each element,
         it is not reflective of the allowed values of the state space in an unterminated episode. Particularly:
     -  The cart x-position (index 0) can be take values between `(-4.8, 4.8)`, but the episode terminates
        if the cart leaves the `(-2.4, 2.4)` range.
     -  The pole angle can be observed between  `(-.418, .418)` radians (or **±24°**), but the episode terminates
        if the pole angle is not in the range `(-.2095, .2095)` (or **±12°**)
+
     ## Rewards
+
     Since the goal is to keep the pole upright for as long as possible, a reward of `+1` for every step taken,
     including the termination step, is allotted. The threshold for rewards is 500 for v1 and 200 for v0.
+    
     ## Starting State
+
     All observations are assigned a uniformly random value in `(-0.05, 0.05)`
+
     ## Episode End
+
     The episode ends if any one of the following occurs:
     1. Termination: Pole Angle is greater than ±12°
     2. Termination: Cart Position is greater than ±2.4 (center of the cart reaches the edge of the display)
     3. Truncation: Episode length is greater than 500 (200 for v0)
+
     ## Arguments
+
     ```python
     import gymnasium as gym
     gym.make('CartPole-v1')
     ```
+
     On reset, the `options` parameter allows the user to change the bounds used to determine
     the new random state.
     """
@@ -191,6 +209,8 @@ class CartPoleEnv_CSC3180(gym.Env[np.ndarray, Union[int, np.ndarray]]):
         return np.array(self.state, dtype=np.float32), {}
 
     def render(self):
+        param1 = 'pole length = ' + str(self.length)
+        param2 = 'pole mass = ' + str(self.masspole)
         if self.render_mode is None:
             assert self.spec is not None
             gym.logger.warn(
@@ -276,8 +296,25 @@ class CartPoleEnv_CSC3180(gym.Env[np.ndarray, Union[int, np.ndarray]]):
 
         gfxdraw.hline(self.surf, 0, self.screen_width, carty, (0, 0, 0))
 
+        font = pygame.font.Font('freesansbold.ttf', 20)
+ 
+        # create a text surface object,
+        # on which text is drawn on it.
+        text1 = font.render(param1, True, (0,0,0))
+        text2 = font.render(param2, True, (0,0,0))
+        
+        # create a rectangular object for the
+        # text surface object
+        textRect1 = text1.get_rect()
+        textRect2 = text2.get_rect()
+        
+        # set the center of the rectangular object.
+        textRect1.center = (self.screen_width // 4, self.screen_height // 8)
+        textRect2.center = (self.screen_width // 4, self.screen_height // 4)
         self.surf = pygame.transform.flip(self.surf, False, True)
         self.screen.blit(self.surf, (0, 0))
+        self.screen.blit(text1, textRect1)
+        self.screen.blit(text2, textRect2)
         if self.render_mode == "human":
             pygame.event.pump()
             self.clock.tick(self.metadata["render_fps"])
@@ -295,6 +332,14 @@ class CartPoleEnv_CSC3180(gym.Env[np.ndarray, Union[int, np.ndarray]]):
             pygame.display.quit()
             pygame.quit()
             self.isopen = False
+
+    def change_poleLength(self, newLength):
+        self.length = newLength
+        self.polemass_length = self.masspole * self.length
+    
+    def change_poleMass(self, newMasspole):
+        self.masspole = newMasspole
+        self.polemass_length = self.masspole * self.length
 
 
 class CartPoleVectorEnv(VectorEnv):
